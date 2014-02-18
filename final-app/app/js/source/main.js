c8be41db6bdaa614d58b7427d829d459ca49d0c4
Mitchell Hollander <MitchellSH@live.com>
2014-02-18 14:59:42 -0600

    home button, search and quiz button

--- final-app/app/js/source/main.js
+++ final-app/app/js/source/main.js
@@ -6,7 +6,55 @@
 
   function initialize(){
     $(document).foundation();
+    $('#home').click(home);
+    $('#jquery').click(jqueryMagic);
+    $('#html-jade').click(htmlMagic);
+    $('#java').click(javaMagic);
+    $('#css').click(cssMagic);
+    $('#lodash').click(lodashMagic);
+    $('#git').click(gitMagic);
   }
+  function createButton(){
+    var test = $('<button>Search</button>');
+    var test2 = $('<button>Quiz</button>');
+
+    $('#container').append(test, test2);
+
+
+  }
+
+  function home(){
+    $('#container').empty();
+  }
+  function jqueryMagic(){
+
+    //$('#jquery').one('click', function(){
+      //var test = $('<button>Search</button>');
+      //var test2 = $('<button>Quiz</button>');
+      //$('#container').append(test, test2);
+    //}
+    createButton();
+  }
+  function htmlMagic(){
+    createButton();
+  }
+
+  function javaMagic(){
+    createButton();
+  }
+
+  function cssMagic(){
+    createButton();
+  }
+
+  function lodashMagic(){
+    createButton();
+  }
+
+  function gitMagic(){
+    createButton();
+  }
+
 
 })();
 
